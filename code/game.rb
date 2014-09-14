dfebf75dcd13dfd4591efd9a5b079d798eeccced
Elia Schito <elia@schito.me>
2014-09-14 15:03:52 +0200

    [snapshot]

--- code/game.rb
+++ code/game.rb
@@ -1,15 +1,22 @@
 class LeoneMangione < Game
   def setup
     size = display.size
-    @center = V[size.x * 0.2, size.y * 0.8]
-    @lion = Lion.new(@center)
+    @horizon = size.y * 0.8
+    @lion = Lion.new(V[size.x * 0.2, horizon])
+    @zebra = Zebra.new(V[size.x * 0.8, horizon])
     @bg = Image.new('bg.png')
+    @animals = [
+      @lion,
+      @zebra,
+    ]
+    draw_bg(display)
   end
 
   def update(elapsed)
-    @lion.update(elapsed)
-    draw_bg(display)
-    @lion.draw(display)
+    @animals.each do |animal|
+      animal.update(elapsed)
+      animal.draw(display)
+    end
   end
 
   def draw_bg(d)
@@ -20,6 +27,36 @@ class LeoneMangione < Game
   end
 end
 
+
+class Lion
+  def initialize(position)
+    @position = position
+    @images = [
+      Image.new('lion-1.png'),
+      Image.new('lion-2.png'),
+    ]
+    @image = @image1
+    @elapsed = 0
+  end
+
+  def update(elapsed)
+    @elapsed += elapsed
+    @elapsed = 0 if @elapsed >= 1
+    @image = @images[(@elapsed*2).to_i]
+  end
+
+  def draw(d)
+    d.push
+
+    # Move "pen" by position.
+    d.translate @position
+
+    # Draw the eyes part of image, centered on pen.
+    d.image(@image, V[0,0])
+
+    d.pop
+  end
+end
 
 class Zebra
   def initialize(position)
